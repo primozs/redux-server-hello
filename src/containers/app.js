35e96deac0b4c34da9b326ece99a3870e9809603
Primoz Susa <primoz.susa@gmail.com>
2015-11-19 22:00:39 +0100

    react init commit

---
+++ src/containers/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+let App = React.createClass({
+  displayName: 'App',
+  render() {
+    return (
+      <div>
+        <h1>hello world</h1>
+      </div>
+    );
+  }
+});
+
+export default App;
